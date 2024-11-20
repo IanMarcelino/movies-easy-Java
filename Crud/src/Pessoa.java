cf9d9158016d403036d094510e66bf4a59ef08e6
Ian Marcelino <iandevcontato@gmail.com>
2024-11-19 21:01:18 -0300

    implementing the actor class

--- Crud/src/Pessoa.java
+++ Crud/src/Pessoa.java
@@ -6,7 +6,7 @@ private String email;
 public Pessoa(String cpf, String nome, String email){
   this.cpf = cpf;
   this.nome = nome; 
-  this.email = email;5
+  this.email = email;
 }
 
 public void setCpf(String cpf){
--- src/Ator.java
+++
@@ -1,3 +0,0 @@
-public class Ator extends Pessoa {
-  
-}
