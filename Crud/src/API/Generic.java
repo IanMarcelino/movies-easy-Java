78db128bca51de2572a7a0633ca579d844dcba8b
Ian Marcelino <iandevcontato@gmail.com>
2024-11-28 18:26:51 -0300

    Método adicionar//cadastro, finalizado e pronto para uso.

--- Crud/src/API/Generic.java
+++ Crud/src/API/Generic.java
@@ -6,6 +6,8 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.*;
+import java.nio.file.*;
 
 // Atributos e Encapsulamento 
   public class Generic<G> {
@@ -20,29 +22,35 @@ import java.io.IOException;
   }
 
 // Métodos Gerais 
-  public boolean register(String dados,String tipoArquivo) {
-    String filepath = "F:\\Crud\\src\\database\\" + tipoArquivo + ".txt";
-      try {
-        //Verificar se os dados já existem no arquivo
-        try (BufferedReader br = new BufferedReader(new FileReader(filepath))) {
-          String line;
-          while ((line = br.readLine()) != null) {
-          if (line.equals(dados)) {
-          System.out.println("Registro já existe: " + dados);
-          return false; // Dados duplicados, não grava
+public boolean register(String dados, String tipoArquivo){
+    String filepath = "C:\\Users\\User\\Desktop\\JAVA_AV3\\Crud\\src\\database\\" + tipoArquivo + ".txt";
+    Path caminhoArquivo = Paths.get(filepath);  
+
+    try {
+        if (!Files.exists(caminhoArquivo)) {
+            Files.createFile(caminhoArquivo);
         }
-    }
+
+        // Verifica se os dados já existem no arquivo
+        try (BufferedReader br = new BufferedReader(new FileReader(caminhoArquivo.toFile()))) {
+            String linha;
+            while ((linha = br.readLine()) != null) {
+                if (linha.equals(dados)) { 
+                    System.out.println("Registro existente: " + dados);
+                    return false; // Dados duplicados, não grava
+                }
+            }
+        }
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(caminhoArquivo.toFile(), true))) {
+            bw.write(dados);
+            bw.newLine();
+            System.out.println("Registro adicionado: " + dados);
+            return true; 
+        }            
+    } catch (IOException e) {
+        System.err.println("Erro ao registrar: " + e.getMessage());
+        return false;
+        }
+
 }
-          // Se não encontrou duplicatas, grava os dados no arquivo
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(filepath, true))) {
-          bw.write(dados);
-          bw.newLine();
-          System.out.println("Registro adicionado: " + dados);
-          return true; // Dados gravados com sucesso
-          }
-        } catch (IOException e) {
-            System.err.println("Erro ao registrar: " + e.getMessage());
-            return false;
-        }
-  }
 }
